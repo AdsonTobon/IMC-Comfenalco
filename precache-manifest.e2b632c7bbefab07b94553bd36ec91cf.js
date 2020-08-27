07ad0cca1d70bd5e1faf39060c309d62cab8fb22
Anderson Tobon <anderson.tobonp@gmail.com>
2020-08-27 12:56:45 -0500

    Updates

---
+++ precache-manifest.e2b632c7bbefab07b94553bd36ec91cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a33746e118b04d88b7ecee2491289d3",
+    "url": "/IMC-Comfenalco/index.html"
+  },
+  {
+    "revision": "54421141366c5d2a6492",
+    "url": "/IMC-Comfenalco/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e6c3aba8de11282b1382",
+    "url": "/IMC-Comfenalco/static/js/2.09d32d23.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/IMC-Comfenalco/static/js/2.09d32d23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54421141366c5d2a6492",
+    "url": "/IMC-Comfenalco/static/js/main.4e06c55e.chunk.js"
+  },
+  {
+    "revision": "399f633427bf1dd6e114",
+    "url": "/IMC-Comfenalco/static/js/runtime-main.ce41c4d3.js"
+  }
+]);
